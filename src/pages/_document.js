a72d807804005478dc41e987592b4429ae5fa63a
AslamBaba <aslam91r@gmail.com>
2025-09-10 04:29:33 +0500

    Fix build bug

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,32 +1,49 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
+          integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+
+        {/* React and ReactDOM */}
+        <Script
+          src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        />
+
+        {/* React Bootstrap */}
+        <Script
+          src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
+        {/* Inline script using dangerouslySetInnerHTML */}
+        <Script
+          id="react-bootstrap-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `var Alert = ReactBootstrap.Alert;`,
+          }}
+        />
       </body>
-
-      <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
-
-      <script
-        src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
-        crossorigin></script>
-
-      <script
-        src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
-        crossorigin></script>
-
-      <script>var Alert = ReactBootstrap.Alert;</script>
-
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
-  integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
-  crossorigin="anonymous"
-/>
     </Html>
   );
 }
